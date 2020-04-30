c20c3a0c6a7d1443ca31c16c11a81cf45cb097fe
Eder Costa <eder.costa@laborit.com.br>
2020-04-29 21:42:24 -0300

    saving status

--- src/application.js
+++ src/application.js
@@ -6,33 +6,33 @@ let canvas = new Canvas('game', 800, 800);
 let pixel = new Pixel(canvas);
 let game = new Game(canvas, pixel);
 
-// Create sprites 
-pixel.loadSprites({
-    plataform : { frames : [rawSprite.plataform], colors : rawSprite.plataformColors},
-    ship : { frames : [rawSprite.ship], colors :rawSprite.shipColors},
-});
+// // Create sprites 
+// pixel.loadSprites({
+//     plataform : { frames : [rawSprite.plataform], colors : rawSprite.plataformColors},
+//     ship : { frames : [rawSprite.ship], colors :rawSprite.shipColors},
+// });
 
-// store animation positions
-let position = { ship : 63, plataform : 60 };
+// // store animation positions
+// let position = { ship : 63, plataform : 60 };
 
-let world_left = 0;
+// let world_left = 0;
 
 game.setGameLoop(() => {
-    if(game.gameLoopCount === 0){
-        pixel.move('plataform', position.plataform, 0);
-        pixel.move('ship', position.ship, 10);    
-    }else{
+    // if(game.gameLoopCount === 0){
+    //     pixel.move('plataform', position.plataform, 0);
+    //     pixel.move('ship', position.ship, 10);    
+    // }else{
         
-        pixel.moveUserView(0, 1);
+    //     pixel.moveUserView(0, 1);
         
-        // if(game.keyPress.up){
-        //     pixel.move('ship', position.ship-=3, 10);
-        // }
+    //     // if(game.keyPress.up){
+    //     //     pixel.move('ship', position.ship-=3, 10);
+    //     // }
 
-        // if(position.ship < 63){
-        //     pixel.move('ship', position.ship++, 10);
-        // }
-    }
+    //     // if(position.ship < 63){
+    //     //     pixel.move('ship', position.ship++, 10);
+    //     // }
+    // }
 });
 
 game.loopStart();
