6a6d9ba9a1ea51017f856a7076557b06a6168c69
Eder Costa <eder.costa@laborit.com.br>
2020-06-20 23:23:19 -0300

    multigame loading

--- src/custom-games/tetris/game.js
+++ src/custom-games/tetris/game.js
@@ -3,55 +3,28 @@ let pixel = new Pixel(canvas);
 let game = new Game(canvas, pixel, 200);
 
 pixel.loadSprites({
-    weirdPeace : { frames : [rawSprites.weirdPeace], colors : rawSprites.weirdPeaceColors},
-    cube : { frames : [rawSprites.cube], colors : rawSprites.cubeColors},
-    t : { frames : [rawSprites.t], colors : rawSprites.tColors},
-    l : { frames : [rawSprites.l], colors : rawSprites.lColors},
-    bar : { frames : [rawSprites.bar], colors : rawSprites.barColors},
-    screen : { frames : [rawSprites.screen], colors : rawSprites.screenColors},
+    stuart : { frames : [rawSprites.stuart], colors : rawSprites.stuartColors},
+    plataform : { frames : [rawSprites.plataform], colors : rawSprites.plataformColors},
+    raio : { frames : [rawSprites.raio], colors : rawSprites.raioColors},
 });
 
-let positionTop = 0;
-let positionLeft = 34;
-let firstPeace = true;
-let peaces = [];
-let peacesIds =[
-    'weirdPeace',
-    'cube',
-    't',
-    'l',
-    'bar',
-];
+pixel.showSprite('stuart');
+pixel.move('stuart', 70, 10);
 
-pixel.move('screen', 70, 10);
-pixel.showSprite('screen');
+pixel.showSprite('raio');
+pixel.move('plataform', 10, 10);
 
-const randStart = peacesIds[Math.floor(Math.random() * peacesIds.length)];
+let positions = {
+    player : { left : 10, top : 60 }
+};
 
 game.setGameLoop(() => {
+    
+    // pixel.move('stuart', positions.player.top--, positions.player.left);
 
-    positionTop+= 5;
-
-    if(positionTop <= 60){
-        if(firstPeace){
-            
-            pixel.showSprite(randStart);
-            pixel.move(randStart, positionTop, positionLeft);
-        }else{
-            pixel.showSprite(peaces[peaces.length - 1]);
-            pixel.move(peaces[peaces.length - 1], positionTop, positionLeft);
-        }
-    }else{
-        firstPeace = false;
-        positionTop = 0;
-
-        const randPeace = peacesIds[Math.floor(Math.random() * peacesIds.length)];
-
-        peaces.push(pixel.cloneSprite(randPeace, 10, 10));
-    }
-        
-    if(game.keyPress.left) positionLeft-=4;
-    if(game.keyPress.right) positionLeft+=4;
+    // if(game.keyPress.left) playerPosition.left-=4;
+    // if(game.keyPress.right) playerPosition.left+=4;
+    // if(game.keyPress.top) playerPosition.top-=10;
 });
 
 game.loopStart();
