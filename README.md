9f74a08b482c5697e0b339ef80a6b5b0f5575bda
Eder Costa <eder.costa@laborit.com.br>
2020-07-01 00:13:11 -0300

    removing grunt for a while

--- README.md
+++ README.md
@@ -7,19 +7,3 @@ the objective is learn about game engines and make some simple games
 ## install 
 
 This is a simple js application just open index.html into a modern browser
-
-## Build Requirements 
-
-npm 6.14.4
-node v10.20.1
-grunt https://gruntjs.com/
-
-## Building 
-
-`npm intall`
-`grunt build`
-
-### Base on  
-
-https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
-
--- src/custom-games/tetris/game.js
+++ src/custom-games/tetris/game.js
@@ -1,57 +1,112 @@
-let canvas = new Canvas('game', 800, 800);
-let pixel = new Pixel(canvas);
-let game = new Game(canvas, pixel, 200);
+class TetrisGame
+{
+    //engine
+    canvas
+    pixel
+    game
 
-pixel.loadSprites({
-    weirdPeace : { frames : [rawSprites.weirdPeace], colors : rawSprites.weirdPeaceColors},
-    cube : { frames : [rawSprites.cube], colors : rawSprites.cubeColors},
-    t : { frames : [rawSprites.t], colors : rawSprites.tColors},
-    l : { frames : [rawSprites.l], colors : rawSprites.lColors},
-    bar : { frames : [rawSprites.bar], colors : rawSprites.barColors},
-    screen : { frames : [rawSprites.screen], colors : rawSprites.screenColors},
-});
+    //game states
+    positionTop = 0;
+    positionLeft = 34;
+    firstPeace = true;
+    peaces = [];
+    peacesIds =[
+        'weirdPeace',
+        'cube',
+        't',
+        'l',
+        'bar',
+    ];
 
-let positionTop = 0;
-let positionLeft = 34;
-let firstPeace = true;
-let peaces = [];
-let peacesIds =[
-    'weirdPeace',
-    'cube',
-    't',
-    'l',
-    'bar',
-];
-
-pixel.move('screen', 70, 10);
-pixel.showSprite('screen');
-
-const randStart = peacesIds[Math.floor(Math.random() * peacesIds.length)];
-
-game.setGameLoop(() => {
-
-    positionTop+= 5;
-
-    if(positionTop <= 60){
-        if(firstPeace){
-            
-            pixel.showSprite(randStart);
-            pixel.move(randStart, positionTop, positionLeft);
-        }else{
-            pixel.showSprite(peaces[peaces.length - 1]);
-            pixel.move(peaces[peaces.length - 1], positionTop, positionLeft);
-        }
-    }else{
-        firstPeace = false;
-        positionTop = 0;
-
-        const randPeace = peacesIds[Math.floor(Math.random() * peacesIds.length)];
-
-        peaces.push(pixel.cloneSprite(randPeace, 10, 10));
+    constructor(){
+        this.canvas = new Canvas('game', 800, 800);
+        this.pixel = new Pixel(this.canvas);
+        this.game = new Game(this.canvas, this.pixel, 200);
+        this.loadSprites();
     }
-        
-    if(game.keyPress.left) positionLeft-=4;
-    if(game.keyPress.right) positionLeft+=4;
-});
 
-game.loopStart();
+    loadSprites(){
+        this.pixel.loadSprites({
+            weirdPeace : { frames : [rawSprites.weirdPeace], colors : rawSprites.weirdPeaceColors},
+            cube : { frames : [rawSprites.cube], colors : rawSprites.cubeColors},
+            t : { frames : [rawSprites.t], colors : rawSprites.tColors},
+            l : { frames : [rawSprites.l], colors : rawSprites.lColors},
+            bar : { frames : [rawSprites.bar], colors : rawSprites.barColors},
+            screen : { frames : [rawSprites.screen], colors : rawSprites.screenColors},
+        });
+    }
+
+    reset(){
+        this.pixel.move('screen', 70, 10);
+        this.pixel.showSprite('screen');
+    }
+
+    getRandPeace = () => this.peacesIds[Math.floor(Math.random() * this.peacesIds.length)]
+
+
+    renderGameFrame = () => {
+        const randStart= this.getRandPeace();
+
+        this.positionTop+= 5;
+
+        if(this.positionTop <= 60){
+            if(this.firstPeace){
+                
+                this.pixel.showSprite(randStart);
+                this.pixel.move(randStart, this.positionTop, this.positionLeft);
+            }else{
+                this.pixel.showSprite(this.peaces[this.peaces.length - 1]);
+                this.pixel.move(this.peaces[this.peaces.length - 1], this.positionTop, this.positionLeft);
+            }
+        }else{
+            this.firstPeace = false;
+            this.positionTop = 0;
+
+            const randPeace = this.peacesIds[Math.floor(Math.random() * this.peacesIds.length)];
+
+            this.peaces.push(this.pixel.cloneSprite(randPeace, 10, 10));
+        }
+            
+        if(this.game.keyPress.left) this.positionLeft-=4;
+        if(this.game.keyPress.right) this.positionLeft+=4;
+    }
+    
+
+    gameStart = () => {
+        this.game.setGameLoop(this.renderGameFrame);
+
+        this.game.loopStart();
+    }
+}
+
+let tetris = new TetrisGame();
+
+tetris.reset();
+tetris.gameStart();
+// let canvas = new Canvas('game', 800, 800);
+// let pixel = new Pixel(canvas);
+// let game = new Game(canvas, pixel, 200);
+
+// pixel.loadSprites({
+//     weirdPeace : { frames : [rawSprites.weirdPeace], colors : rawSprites.weirdPeaceColors},
+//     cube : { frames : [rawSprites.cube], colors : rawSprites.cubeColors},
+//     t : { frames : [rawSprites.t], colors : rawSprites.tColors},
+//     l : { frames : [rawSprites.l], colors : rawSprites.lColors},
+//     bar : { frames : [rawSprites.bar], colors : rawSprites.barColors},
+//     screen : { frames : [rawSprites.screen], colors : rawSprites.screenColors},
+// });
+
+// let positionTop = 0;
+// let positionLeft = 34;
+// let firstPeace = true;
+// let peaces = [];
+// let peacesIds =[
+//     'weirdPeace',
+//     'cube',
+//     't',
+//     'l',
+//     'bar',
+// ];
+
+
+// const randStart = peacesIds[Math.floor(Math.random() * peacesIds.length)];
