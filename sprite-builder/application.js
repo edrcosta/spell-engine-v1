4bcaa361f6f3ea9ecea998fced6ddbf92691457c
Eder Costa <eder.costa@laborit.com.br>
2020-07-02 22:27:16 -0300

    sprite builder

--- sprite-builder/application.js
+++ sprite-builder/application.js
@@ -1,7 +1,7 @@
 let draw = document.getElementById('draw-area');
 
-const colsNum = 20;
-const rowsNum = 20;
+const colsNum = 100;
+const rowsNum = 30;
 
 let grid = [];
 
@@ -12,9 +12,30 @@ for (let i = 0; i < rowsNum; i++) {
     div = `<div class="row">${div.join('')}</div>`;
 
     grid.push(div);
-    
 }
 
-
-
 draw.innerHTML = grid.join('');
+
+let pixelsDivs = document.getElementsByClassName('pixel');
+
+for (let i = 0; i < pixelsDivs.length; i++) {
+    pixelsDivs[i].onclick = () =>{
+        pixelsDivs[i].style.background = document.getElementById('color-input').value;
+    }
+}
+
+document.getElementById('import-data').onclick = () => {
+    
+    let pixelsDivs = document.getElementsByClassName('pixel');
+    let rowsDivs = document.getElementsByClassName('row');
+
+    for (let r = 0; r < rowsDivs.length; r++) {
+        const row = rowsDivs[r];
+
+        for (let c = 0; c < pixelsDivs.length; c++) {
+            const pixel = pixelsDivs[c];
+            
+            console.log(pixel.style.background);
+        }
+    }
+}
--- src/custom-games/tetris/game.js
+++ src/custom-games/tetris/game.js
@@ -5,10 +5,18 @@ class TetrisGame
     pixel
     game
 
-    //game states
+    //Positions
     positionTop = 0;
     positionLeft = 34;
+    
+    //Bottom stack mapping
+    bottomMap = Array(15).fill(0);
+    bottonMapPointer = 6;
+    
+    //peace control
+    currentPeace ='';
     firstPeace = true;
+    
     peaces = [];
     peacesIds =[
         'weirdPeace',
@@ -37,30 +45,103 @@ class TetrisGame
 
         //loading bitmap sprites into memory 
         this.pixel.loadSprites(this.sprites);
+
+        this.randStart = this.getRandPeace();
     }
 
-    reset(){
+    //Reset the game to the start states
+    reset = () => {
         //render basic game ui
-        this.pixel.move('screen', 70, 10);
         this.pixel.showSprite('screen');
-
-        this.pixel.move('screenVertical', 10, 10);
         this.pixel.showSprite('screenVertical');
 
-        
+        this.pixel.move('screen', 70, 10);
+        this.pixel.move('screenVertical', 10, 10);
+        //Positions
+        this.positionTop = 0;
+        this.positionLeft = 34;
+        //Bottom stack mapping
+        this.bottomMap = Array(15).fill(0);
+        this.bottonMapPointer = 6;
+        //peace control
+        this.currentPeace ='';
+        this.firstPeace = true;
     }
 
-    getRandPeace = () => this.peacesIds[Math.floor(Math.random() * this.peacesIds.length)]
+    //get a randon tetris peace 
+    getRandPeace = () => this.peacesIds[Math.floor(Math.random() * this.peacesIds.length)];
 
-    gameFrame = () => {
+    //fires every frame when a peace is falling down
+    gameStepDropPeace = () => {
 
-        // console.log(this.getRandPeace());
-        this.pixel.showSprite('cube');
-        this.pixel.move('cube', 10, 10);
+        const peace = this.firstPeace ? this.randStart : this.peaces[this.peaces.length - 1];
+
+        this.pixel.showSprite(peace);
+        this.pixel.move(peace, this.positionTop, this.positionLeft);        
+    }
+    
+    mapPeaceBottonStack = () =>{
+        if(this.currentPeace.length > 0){
+            this.bottomMap[this.bottonMapPointer] += this.getPeaceSize(this.currentPeace);
+        }
+    }
+
+    //Create a new peace when another get to the botton
+    //fires when a peace gets to the botton and a new one has to be created 
+    gameStepPeaceGetToThebotton = () => {
+    
+        this.mapPeaceBottonStack();
+        
+        this.firstPeace = false;
+        this.positionTop = 0;
+
+        this.currentPeace = this.getRandPeace();
+
+        this.peaces.push(this.pixel.cloneSprite(this.currentPeace, 10, 10));
+    }
+
+    getPeaceSize = (peace) => {
+        return 2;
+        // if(peace === 'cube') return 6;
+        // if(peace === 'weirdPeace') return 6;
+        // if(peace === 'bar') return 15;
+        // if(peace === 'l') return 11;
+        // if(peace === 't') return 6;
+        // else throw 'invalid form::' + peace;
+    }
+
+    //game logic for peace droping
+    gameStepDropPeaces = () => {
+        this.positionTop+= 5;
+
+        console.log(this.bottomMap)
+
+        const botton = 60 - this.bottomMap[this.bottonMapPointer];
+
+        if(this.positionTop <= botton){
+            this.gameStepDropPeace();
+        }else{
+            this.gameStepPeaceGetToThebotton();
+        }
+    }
+
+    gameLoop = () => {
+
+        this.gameStepDropPeaces();
+
+        if(this.game.keyPress.left){
+            this.positionLeft-=4;
+            this.bottonMapPointer--;
+        }
+        
+        if(this.game.keyPress.right){
+            this.positionLeft+=4;
+            this.bottonMapPointer++;
+        }
     }
 
     gameStart = () => {
-        this.game.setGameLoop(this.gameFrame);
+        this.game.setGameLoop(this.gameLoop);
         this.game.loopStart();
     }
 }
@@ -69,56 +150,3 @@ let tetris = new TetrisGame();
 
 tetris.reset();
 tetris.gameStart();
-// let canvas = new Canvas('game', 800, 800);
-// let pixel = new Pixel(canvas);
-// let game = new Game(canvas, pixel, 200);
-
-// pixel.loadSprites({
-//     weirdPeace : { frames : [rawSprites.weirdPeace], colors : rawSprites.weirdPeaceColors},
-//     cube : { frames : [rawSprites.cube], colors : rawSprites.cubeColors},
-//     t : { frames : [rawSprites.t], colors : rawSprites.tColors},
-//     l : { frames : [rawSprites.l], colors : rawSprites.lColors},
-//     bar : { frames : [rawSprites.bar], colors : rawSprites.barColors},
-//     screen : { frames : [rawSprites.screen], colors : rawSprites.screenColors},
-// });
-
-// let positionTop = 0;
-// let positionLeft = 34;
-// let firstPeace = true;
-// let peaces = [];
-// let peacesIds =[
-//     'weirdPeace',
-//     'cube',
-//     't',
-//     'l',
-//     'bar',
-// ];
-
-
-// const randStart = peacesIds[Math.floor(Math.random() * peacesIds.length)];
-
-
-//  const randStart= this.getRandPeace();
-    
-//             this.positionTop+= 5;
-    
-//             if(this.positionTop <= 60){
-//                 if(this.firstPeace){
-                    
-//                     this.pixel.showSprite(randStart);
-//                     this.pixel.move(randStart, this.positionTop, this.positionLeft);
-//                 }else{
-//                     this.pixel.showSprite(this.peaces[this.peaces.length - 1]);
-//                     this.pixel.move(this.peaces[this.peaces.length - 1], this.positionTop, this.positionLeft);
-//                 }
-//             }else{
-//                 this.firstPeace = false;
-//                 this.positionTop = 0;
-    
-//                 const randPeace = this.peacesIds[Math.floor(Math.random() * this.peacesIds.length)];
-    
-//                 this.peaces.push(this.pixel.cloneSprite(randPeace, 10, 10));
-//             }
-                
-//             if(this.game.keyPress.left) this.positionLeft-=4;
-//             if(this.game.keyPress.right) this.positionLeft+=4;
